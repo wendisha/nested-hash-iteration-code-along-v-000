d696c4a7d5eb4641a2fd86ad30cff21b9b55596c
Wendy Calderon <wendycalderon@gmail.com>
2018-06-16 17:20:34 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, contacts_details_hash|
     contacts_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
-        binding.pry
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
       end
     end
   end   
